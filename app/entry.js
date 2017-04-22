f2d8fbc8789222e5326b43a561ea5643cbfb1bbc
kcirekcom <erick.f.mock@gmail.com>
2017-04-22 15:07:44 -0700

    setup entry file

---
+++ app/entry.js
@@ -0,0 +1,32 @@
+'use strict';
+
+require('./scss/main.scss');
+
+const path = require('path');
+const angular = require('angular');
+const camelcase = require('camelcase');
+const pascalcase = require('pascalcase');
+const uiRouter = require('angular-ui-router');
+const ngTouch = require('angular-touch');
+const ngAnimate = require('angular-animate');
+
+const mrsnovel = angular.module('mrsnovel', [ngTouch, ngAnimate, uiRouter]);
+
+let context = require.context('./config/', true, /\.js$/);
+context.keys().forEach(path => {
+  mrsnovel.config(context(path));
+});
+
+context = require.context('./view/', true, /\.js$/);
+context.keys().forEach(key => {
+  let name = pascalcase(path.basename(key, '.js'));
+  let module = context(key);
+  mrsnovel.controller(name, module);
+});
+
+context = require.context('./component/', true, /\.js$/);
+context.keys().forEach(key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  mrsnovel.component(name, module);
+});
