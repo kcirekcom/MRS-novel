ffbb981e0288970cfb4b6121b3099e8981e8e14b
kcirekcom <erick.f.mock@gmail.com>
2017-04-24 20:05:53 -0700

    edit-manuscript component built out

---
+++ app/component/manuscript/edit-manuscript/edit-manuscript.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  template: require('./edit-manuscript.html'),
+  controller: ['$log', 'manuscriptService', EditManuscriptController],
+  controllerAs: 'editManuscriptCtrl',
+  bindings: {
+    manuscript: '<'
+  }
+};
+
+function EditManuscriptController($log, manuscriptService) {
+  $log.debug('EditManuscriptController');
+
+  this.updateManuscript = function() {
+    $log.debug('editManuscriptCtrl.updateManuscript()');
+    manuscriptService.updateManuscript(this.manuscript)
+    .then(() => {
+      $log.debug('manuscript updated');
+    })
+    .catch( err => {
+      $log.error(err.message);
+    });
+  };
+}
