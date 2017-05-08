046f6a017eabfb1a98ce39c49633dc223155c475
kcirekcom <erick.f.mock@gmail.com>
2017-05-07 21:21:09 -0600

    styling to chapter item component

--- app/component/manuscript/chapter-item/chapter-item.js
+++ app/component/manuscript/chapter-item/chapter-item.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   template: require('./chapter-item.html'),
-  controller: ['$log', 'chapterService', ChapterItemController],
+  controller: ['$log', '$window', 'chapterService', ChapterItemController],
   controllerAs: 'chapterItemCtrl',
   bindings: {
     chapter: '<',
@@ -10,7 +10,7 @@ module.exports = {
   }
 };
 
-function ChapterItemController($log, chapterService) {
+function ChapterItemController($log, $window, chapterService) {
   $log.debug('ChapterItemController');
 
   this.showEditChapter = false;
@@ -21,7 +21,6 @@ function ChapterItemController($log, chapterService) {
 
   this.words;
 
-
   this.readLess = function() {
     this.numLimit = 2000;
   };
@@ -29,7 +28,7 @@ function ChapterItemController($log, chapterService) {
   this.readMore = function() {
     this.numLimit = 100000;
   };
-  
+
   this.wordCount = function() {
     $log.debug('chapterItemCtrl.wordCount()');
     let str = this.chapter.body.replace(/<\/?[a-z][^>]*>/gi, '');
