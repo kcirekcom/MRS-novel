16d183e715224bf5b869b857a380332a1f455cc9
kcirekcom <erick.f.mock@gmail.com>
2017-07-17 13:46:40 -0600

    cleaning up chapter code

--- app/component/manuscript/chapter/chapter.js
+++ app/component/manuscript/chapter/chapter.js
@@ -13,18 +13,6 @@ module.exports = {
 function ChapterController($log, $window) {
   $log.debug('ChapterController');
 
-  this.showReadMore = false;
-
-  this.numLimit = 2000;
-
-  this.readLess = function() {
-    this.numLimit = 2000;
-  };
-
-  this.readMore = function() {
-    this.numLimit = 100000;
-  };
-
   this.scrollUp = function() {
     $window.scrollTo(100, 0);
   };
